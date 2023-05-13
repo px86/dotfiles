de4b856e9759ada484688115e531a1f5c24fb3c6
Pushkar Raj <px86@protonmail.com>
2023-05-13 14:51:26 +0530

    Update qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,4 +1,4 @@
-'''px86's qtile configuration.'''
+"""px86's qtile configuration."""
 
 #
 #     __ _| |_(_) | ___
@@ -8,29 +8,21 @@
 #       |_|
 #
 
-# ---------------------------------------------------------------------------
-# IMPORTS
-# ---------------------------------------------------------------------------
-
-from libqtile import layout, hook
+from libqtile import layout
 from libqtile.lazy import lazy
-from libqtile.config import EzKey as Key
 from libqtile.config import Click, Drag, Screen, Match
 
-from keybindings import keys, Mod, modifier_keys
+from keybindings import keys, MOD, modifier_keys
 from groups import groups
-from styles import background as bg
+from styles import bg
 from bar import bar, widget_defaults
 
-# -----------------------------------------------------------------------
-# LAYOUTS
-# -----------------------------------------------------------------------
 
 layouts = [
     layout.MonadTall(
         align=layout.MonadTall._left,
-        border_focus=bg['border'],
-        border_normal=bg['bar'],
+        border_focus=bg["border"],
+        border_normal=bg["bar"],
         border_width=2,
         ratio=0.60,
         change_ratio=0.05,
@@ -38,14 +30,14 @@ layouts = [
         max_ratio=0.75,
         min_ratio=0.25,
         min_secondary_size=85,
-        new_client_position='after_current',
+        new_client_position="after_current",
         single_border_width=0,
         margin=0,
         single_margin=0,
     ),
     layout.Columns(
-        border_focus=bg['border'],
-        border_normal=bg['bar'],
+        border_focus=bg["border"],
+        border_normal=bg["bar"],
         fair=False,
         num_columns=2,
         border_width=2,
@@ -56,46 +48,56 @@ layouts = [
     layout.Max(),
 ]
 
+floating_layout = layout.Floating(
+    border_width=3,
+    border_focus=bg["border"],
+    border_normal=bg["bar"],
+    float_rules=[
+        *layout.Floating.default_float_rules,
+        Match(wm_class="GNU Octave"),
+        Match(wm_class="Gcolor3"),
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="pinentry"),  # GPG key password entry
+        Match(wm_class="pinentry-gtk-2"),  # GPG key password entry
+        Match(title="branchdialog"),  # gitk
+        Match(title="float-me"),  # Float certain TUI applications
+    ],
+)
+
 # SCREEN #
 screens = [Screen(top=bar)]
 
 mouse = [
-    Drag([Mod], 'Button1', lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([Mod], 'Button3', lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([Mod], 'Button2', lazy.window.bring_to_front()),
+    Drag(
+        [MOD],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [MOD],
+        "Button3",
+        lazy.window.set_size_floating(),
+        start=lazy.window.get_size(),
+    ),
+    Click(
+        [MOD],
+        "Button2",
+        lazy.window.bring_to_front(),
+    ),
 ]
-
-floating_layout = layout.Floating(
-    border_width=3,
-    border_focus=bg['border'],
-    border_normal=bg['bar'],
-    float_rules=[
-        *layout.Floating.default_float_rules,
-        Match(wm_class='GNU Octave'),
-        Match(wm_class='Gcolor3'),
-        Match(wm_class='confirmreset'),  # gitk
-        Match(wm_class='makebranch'),    # gitk
-        Match(wm_class='maketag'),       # gitk
-        Match(wm_class='ssh-askpass'),   # ssh-askpass
-        Match(wm_class='pinentry'),      # GPG key password entry
-        Match(title='branchdialog'),     # gitk
-        Match(title='float-me'),         # Float certain TUI applications
-    ])
 
 dgroups_key_binder = None
 dgroups_app_rules = []
 follow_mouse_focus = True
-bring_front_click = 'floating_only'
+bring_front_click = "floating_only"
 reconfigure_screens = True
 cursor_warp = True
 extension_defaults = widget_defaults.copy()
 auto_fullscreen = True
 auto_minimize = True
-focus_on_window_activation = 'smart'
-wmname = 'LG3D'
-
-# --------------------------------------------------------------------------
-# END
-# --------------------------------------------------------------------------
+focus_on_window_activation = "smart"
+wmname = "LG3D"
--- .config/qtile/groups.py
+++ .config/qtile/groups.py
@@ -1,31 +1,50 @@
-'''Groups and dropdowns for my Qtile WM session.'''
+"""Groups and dropdowns for my Qtile configuration."""
 
-from libqtile.config import Group, ScratchPad, DropDown, Match
+from libqtile.config import Group
+from libqtile.config import ScratchPad
+from libqtile.config import DropDown
+from libqtile.config import Match
 
 # Window class names
-_browsers = ['firefox', 'Firefox-esr', 'brave-browser', 'chromium']
-_communication = ['walc', 'TelegramDesktop']
+_web    = ["firefox", "Firefox-esr", "brave-browser", "chromium"]
+_docs   = ["Zathura", "libreoffice"]
+_music  = ["deadbeef"]
+_pics   = ["gimp.bin"]
+_comm   = ["walc", "TelegramDesktop"]
 
-groups = [
-    Group('1', label=''),
-    Group('2', label='', matches=[
-        Match(wm_class=_browsers)
-    ]),
-    Group('3', label='', matches=[
-        Match(wm_class=['Zathura', 'libreoffice']),
-    ]),
-    Group('4', label='', matches=[
-        Match(wm_class=['deadbeef'])
-    ]),
-    Group('5', label=''),
-    Group('6', label='', layout='max', matches=[
-        Match(wm_class=['gimp.bin']),
-    ]),
-    Group('7', label='', matches=[
-        Match(wm_class=_communication),
-    ]),
-    ScratchPad(name='scratchpad', dropdowns=[
-        DropDown(name='term', cmd='xterm -T dropdown -fullscreen',
-                 x=0, y=0, width=1, height=1, opacity=0.95),
-    ]),
+groups: list[Group] = [
+    Group("1", label=""),
+    Group("2", label="", matches=[Match(wm_class=_web)]),
+    Group(
+        "3",
+        label="",
+        matches=[Match(wm_class=_docs)],
+    ),
+    Group("4", label="", matches=[Match(wm_class=_music)]),
+    Group("5", label=""),
+    Group(
+        "6",
+        label="",
+        layout="max",
+        matches=[Match(wm_class=_pics)],
+    ),
+    Group(
+        "7",
+        label="",
+        matches=[Match(wm_class=_comm)],
+    ),
+    ScratchPad(
+        name="scratchpad",
+        dropdowns=[
+            DropDown(
+                name="term",
+                cmd="xterm -T dropdown -fullscreen",
+                x=0,
+                y=0,
+                width=1,
+                height=1,
+                opacity=0.95,
+            ),
+        ],
+    ),
 ]
